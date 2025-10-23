0a7807fd561c7ba23c85a9b7f0e48d7f94f95a65
Sadhwika <sadhwikarachamalla@gmail.com>
2025-10-23 12:08:57 -0400

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://sadhwika592.github.io/Angulardemo',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
